f3b8669d5564db10ef64ab303de1b78323c15783
SantGT5 <gianspf@gmail.com>
2022-05-20 19:56:28 +0200

    Enzyme config && Test Congrats component

---
+++ src/Congrats.test.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+import Enzyme, { shallow } from "enzyme";
+import EnzymeAdapter from "@wojtekmaj/enzyme-adapter-react-17";
+import { findByTestAttr } from "../test/testUtils";
+import Congrats from "./Congrats";
+
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+const setup = (props = {}) => {
+  return shallow(<Congrats {...props} />);
+};
+
+test("renders without error", async () => {
+  const wrapper = setup();
+  const component = await findByTestAttr(wrapper, "component-congrats");
+  expect(component.length).toBe(1)
+});
+
+test("Renders no text when `success` prop is false", async () => {
+    const wrapper = setup({ success: false })
+    const component = await findByTestAttr(wrapper, "component-congrats");
+    expect(component.text()).toBe('')
+});
+
+test("renders non-empty congrats message when success prop is true", async() => {
+    const wrapper = setup({ success: true })
+    const message = await findByTestAttr( wrapper, 'congrats-message' )
+    expect(message.text().length).not.toBe(0)
+});
